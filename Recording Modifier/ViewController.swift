c0db0ac0596bb1ef8444d71c1cee24e2c4caf168
Rachel Thomas <racheltho@gmail.com>
2014-12-29 17:20:10 +1100

    Initial Commit

---
+++ Recording Modifier/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Recording Modifier
+//
+//  Created by Rachel Thomas on 12/29/14.
+//  Copyright (c) 2014 Rachel Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
