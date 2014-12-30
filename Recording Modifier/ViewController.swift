c438c5c9d346c0e3ddef2dc83c261d7b6fb5d752
Rachel Thomas <racheltho@gmail.com>
2014-12-30 21:01:46 +1100

    added recording and stop buttons with label, not yet functional

--- Recording Modifier/ViewController.swift
+++ Recording Modifier/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingInProgress: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: UIButton) {
+        recordingInProgress.hidden = false
+        //TODO: Record the user's voice
+        println("in recordAudio")
+    }
 
 }
 
